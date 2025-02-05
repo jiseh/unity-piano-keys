5085c249ac01107bd6b3dee93831b4edd2cde44c
jiseh <josejirehevite@gmail.com>
2025-02-05 23:02:19 +0800

    Implement simple timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Timer : MonoBehaviour
+{
+    public bool TimerActive { get { return _timerActive; } }
+
+    [Header("Debugging")]
+    [SerializeField] private bool _timeLogging;
+
+    private UnityEvent OnTimerEnd = new();
+    private float _currentTime;
+    private bool _timerActive;
+
+    private void Update()
+    {
+        HandleTimer();
+    }
+
+    public void StartTimer(float duration, bool overrideTimerInProgress = false)
+    {
+        if (_timerActive)
+        {
+            Debug.LogWarning($"Timer is active with {_currentTime} left");
+            if (!overrideTimerInProgress)
+            {
+                Debug.LogWarning($"StartTimer aborted, parameters was not set to override timer in progress");
+                return;
+            }
+        } 
+        _currentTime = duration;
+        _timerActive = true;
+    }
+
+    public void AddListenerToOnTimerEnd(UnityAction unityAction)
+    {
+        OnTimerEnd.AddListener(unityAction);
+    }
+
+    public void RemoveListenerToOnTimerEnd(UnityAction unityAction)
+    {
+        OnTimerEnd.RemoveListener(unityAction);
+    }
+
+    private void HandleTimer()  
+    {
+        if (!_timerActive)
+            return;
+        _currentTime -= Time.deltaTime;
+        TryLoggingCurrentTime();
+        if (_currentTime <= 0)
+        {
+            _timerActive = false;
+            OnTimerEnd?.Invoke();
+        }
+    }
+
+    private void TryLoggingCurrentTime()
+    {
+        if(_timeLogging)
+            Debug.Log($"CurrentTime: {_currentTime}");
+    }
+
+    #region Debugger
+    [ContextMenu("StartTimerFiveSeconds")]
+    private void StartTimerFiveSeconds()
+    {
+        StartTimer(5f, false);
+    }
+
+    [ContextMenu("StartTimerFiveSecondsOverride")]
+    private void StartTimerTenSecondsOverride()
+    {
+        StartTimer(10f, true);
+    }
+    #endregion
+}
